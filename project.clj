ec6b81ab0265c59ca8562120a8c833e284321a87
Mark McGranaghan <mmcgrana@gmail.com>
2010-01-01 09:42:55 -0800

    Initial extraction from FleetDB.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject fleetdb-client "0.1.0-SNAPSHOT"
+  :description "A Clojure client for FleetDB."
+  :url "http://github.com/mmcgrana/fleetdb-client"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [clj-json "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [clj-unit "0.1.0-SNAPSHOT"]])
